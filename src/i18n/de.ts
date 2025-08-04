7757a0bd6f03adb15b0e669ed987d7182d06ceb6
Dori Fdez <md.fernandez.sanchez@gmail.com>
2025-08-04 12:43:23 +0200

    Improve German version

--- src/i18n/de.ts
+++ src/i18n/de.ts
@@ -35,8 +35,8 @@ export default {
   },
   contact: {
     message: "Zögere nicht, mir zu schreiben oder mich direkt zu kontaktieren!",
-    email: "📧 Telefon: +34 623 989 165 / +49 176 47157247",
-    phone: "📞 E-Mail: md.fernandez.sanchez@gmail.com"
+    email: "📞 Telefon: +34 623 989 165 / +49 176 47157247",
+    phone: "📧 E-Mail: md.fernandez.sanchez@gmail.com"
   },
   
   nav: {
